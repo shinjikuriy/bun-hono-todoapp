b5652df26a21c6b541872792ee83ba4a70fedd73
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2024-11-29 21:41:49 +0900

    init frontend

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import { Hono } from 'hono'
+import { serveStatic } from 'hono/bun'
+
+const app = new Hono()
+
+app.use(
+  '/*',
+  serveStatic({
+    root: './',
+    rewriteRequestPath: (path) => path.replace(/\/static/, 'dist/static'),
+  })
+)
+app.get('/', (c) => {
+  return c.html(
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+        <script type="module" src="/static/client.js"></script>
+      </head>
+      <body></body>
+    </html>
+  )
+})
+
+export default app
