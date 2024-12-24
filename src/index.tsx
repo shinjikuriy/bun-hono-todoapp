4b5ac5306bf8ca727a3d4e400e224b4591cd3221
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2024-12-24 18:22:26 +0900

    read different client script on dev env

--- src/index.tsx
+++ src/index.tsx
@@ -16,8 +16,12 @@ app.get('/', (c) => {
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
-        <script type="module" src="/static/client.js"></script>
+        <title>Todo App</title>
+        {import.meta.env.PROD ? (
+          <script type="module" src="/static/client.js"></script>
+        ) : (
+          <script type="module" src="/src/client.tsx"></script>
+        )}
       </head>
       <body></body>
     </html>
